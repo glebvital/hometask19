8baf1e14585d1784b39493c1937ba41a5d7109ae
gleb <git config --global user.email>
2024-07-06 19:55:59 +0000

    timer and stopwatch cool files!!!

---
+++ src/main/java/org/example/Stopwatch.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import javax.swing.*;
+
+class Stopwatch extends Thread{
+
+    private JTextField stopwatchText;
+    private boolean booleanvalue;
+    private int i = 0;
+
+    public Stopwatch(JTextField stopwatchText) {
+        this.stopwatchText = stopwatchText;
+    }
+
+    public boolean isBooleanvalue() {
+        return booleanvalue;
+    }
+
+    public void setBooleanvalue(boolean booleanvalue) {
+        this.booleanvalue = booleanvalue;
+    }
+
+    @Override
+    public void run() {
+        stopwatch();
+    }
+
+    public void stopwatch(){
+        while (booleanvalue) {
+            String sec = String.valueOf(i % 60);
+            String min = String.valueOf((i/60) % 60);
+            String hour = String.valueOf((i/3600) % 24);
+            String days = String.valueOf(i/86400);
+            stopwatchText.setText(days + ":" + hour + ":" + min + ":" + sec);
+            i++;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
